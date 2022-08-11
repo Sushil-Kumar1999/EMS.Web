f7e294e6df7ea79f95f2690848e574fd93de3a65
Sushil Kumar <johnklutz.sk@gmail.com>
2022-08-11 09:50:42 +0100

    small change

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -13,14 +13,11 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // Get the auth token.
-        const authToken = `Bearer ${localStorage.getItem('token')}`;
-
         // Clone the request and replace the original headers with
         // cloned headers, updated with the authorization.
         const authReq = req.clone({
             setHeaders: {
-                'Authorization': authToken,
+                'Authorization': `Bearer ${localStorage.getItem('token')}`,
             }
         });
         
