22c8050d61b42809ff0d5682099dd1429394b990
Sushil Kumar <johnklutz.sk@gmail.com>
2022-08-15 18:31:48 +0100

    load events for display correctly

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { jqxSchedulerComponent } from 'jqwidgets-ng/jqxscheduler';
 import { IEvent } from 'src/app/models/event.model';
 import { EventsService } from 'src/app/services/events.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-calendar',
@@ -13,7 +14,7 @@ export class CalendarComponent implements AfterViewInit {
   views: string[] = [ "dayView", "weekView", "monthView"];
   source: any =
   {
-    dataType: "array",
+    dataType: 'array',
     dataFields: [
       { name: 'id', type: 'number' },
       { name: 'description', type: 'string' },
@@ -22,10 +23,11 @@ export class CalendarComponent implements AfterViewInit {
       { name: 'startDate', type: 'date' },
       { name: 'endDate', type: 'date' }
     ],
-    id: 'id'
+    id: 'id',
+    localData: null
   };
+
   dataAdapter: any = new jqx.dataAdapter(this.source);
-  //date: any = new jqx.date(2022, 8, 12);
   appointmentDataFields: jqwidgets.SchedulerAppointmentDataFields =
   {
       from: "startDate",
@@ -44,22 +46,38 @@ export class CalendarComponent implements AfterViewInit {
 
   getEvents() {
     this.eventsService.listEvents()
-    .subscribe(events => 
-      events.forEach(e => this.scheduler.addAppointment(e))
-    );
+      .subscribe(events => {
+        this.scheduler.beginAppointmentsUpdate();
+        this.source.localData = events;
+        this.dataAdapter = new jqx.dataAdapter(this.source);
+        this.scheduler.endAppointmentsUpdate();      
+      });
   }
 
-  // called when the dialog is craeted.
   editDialogCreate(dialog?: jqwidgets.SchedulerEditDialogCreate['dialog'],
                   fields?: jqwidgets.SchedulerEditDialogCreate['fields'],
                   editAppointment?: jqwidgets.SchedulerEditDialogCreate['editAppointment']) {
-    fields.repeatContainer.hide();
-    fields.statusContainer.hide();
-    fields.timeZoneContainer.hide();
-    fields.colorContainer.hide();
-    fields.subjectLabel.html("Title");
-    fields.locationLabel.html("Location");
-    fields.fromLabel.html("Start Date");
-    fields.toLabel.html("End Date");
+    fields?.repeatContainer.hide();
+    fields?.statusContainer.hide();
+    fields?.timeZoneContainer.hide();
+    fields?.colorContainer.hide();
+    fields?.subjectLabel.html("Title");
+    fields?.locationLabel.html("Location");
+    fields?.fromLabel.html("Start Date");
+    fields?.toLabel.html("End Date");
   };
+
+  createNewEvent(event: any): void{
+    let appointment = event.args.appointment.originalData;
+    console.log(appointment);
+    // this.eventsService.createEvent(event)
+    //   .subscribe(() => {
+    //     Swal.fire({
+    //       title: 'Success',
+    //       text: 'New event created successfully',
+    //       icon: 'success',
+    //       showCloseButton: true
+    //     });
+    //   });
+  }
 }
