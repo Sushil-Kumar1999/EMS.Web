05af32a27b954e588876dc4ceddddfb6bbaf0c20
Sushil Kumar <johnklutz.sk@gmail.com>
2022-08-15 16:10:37 +0100

    fixed issue with navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -13,20 +14,21 @@ export class NavbarComponent implements OnInit {
   role = '';
 
   menu_options: MenuOption[] = [];
-  selected: number = -1;
+  selected?: number = -1;
 
-  constructor(private authService: AuthService) { 
+  constructor(private authService: AuthService, private router: Router) { 
     this.userName = localStorage.getItem('loginEmail');
-  }
+  }   
 
   ngOnInit(): void {
     this.menu_options.push(
-      {id: 0, url: '/dashboard', icon: 'dashboard', label: 'Dashboard', selected: true},
+      {id: 0, url: '/dashboard', icon: 'dashboard', label: 'Dashboard', selected: false},
       {id: 1, url: '/calendar', icon: 'event', label: 'Calendar', selected: false},
       {id: 2, url: '/users', icon: 'supervisor_account', label: 'Users', selected: false},
       {id: 3, url: '/events', icon: 'event', label: 'Events', selected: false},
     );
-    this.selected = this.selected  = 1 ? 0 : this.selected;
+    let option = this.menu_options.find(m => m.url == this.router.url) as MenuOption;
+    option.selected = true;
   }
 
   logout(){
